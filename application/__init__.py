c2099b9770e34d35449f7ec23b7f3bd40684ece6
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-20 14:44:52 -1000

    Convert html bootstrap forms to flask-bootstrap form.

--- application/__init__.py
+++ application/__init__.py
@@ -8,54 +8,18 @@ from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     SelectField
 from wtforms.validators import Required
 
-
-# straight from the wtforms docs:
-class TelephoneForm(Form):
-    country_code = IntegerField('Country Code', [validators.required()])
-    area_code = IntegerField('Area Code/Exchange', [validators.required()])
-    number = TextField('Number')
-
-
-class ExampleForm(Form):
-    # field1 = TextField('First Field', description='This is field one.')
+class SearchForm(Form):
+    # Dropdowns
     x_pol = SelectField(label='X Pol Parameters', choices=[('a', 'apes'), ('b', 'bananas'), ('c', 'crabs')])
     y_pol = SelectField(label='Y Pol Parameters', choices=[('a', 'apes'), ('b', 'bananas'), ('c', 'crabs')])
     gen_sys = SelectField(label='General System Parameters', choices=[('a', 'apes'), ('b', 'bananas'), ('c', 'crabs')])
 
+    # datetimepicker http://eonasdan.github.io/bootstrap-datetimepicker/
     begin = DateField(id='begin_dtpicker', label='Begin:')
     end = DateField(id='end_dtpicker', label='End:')
 
-    # field2 = TextField('Second Field', description='This is field two.',
-    #                    validators=[Required()])
-    # hidden_field = HiddenField('You cannot see this', description='Nope')
-    # recaptcha = RecaptchaField('A sample recaptcha field')
-    # radio_field = RadioField('This is a radio field', choices=[
-    #     ('head_radio', 'Head radio'),
-    #     ('radio_76fm', "Radio '76 FM"),
-    #     ('lips_106', 'Lips 106'),
-    #     ('wctr', 'WCTR'),
-    # ])
-    # checkbox_field = BooleanField('This is a checkbox',
-    #                               description='Checkboxes can be tricky.')
-    #
-    # # subforms
-    # mobile_phone = FormField(TelephoneForm)
-    #
-    # # you can change the label as well
-    # office_phone = FormField(TelephoneForm, label='Your office phone')
-    #
-    # ff = FileField('Sample upload')
-
-    submit_button = SubmitField('Send')
-
-
-    def validate_hidden_field(form, field):
-        raise ValidationError('Always wrong')
-
-# class DateTimePicker(Form):
-#     """datetimepicker http://eonasdan.github.io/bootstrap-datetimepicker/
-#     """
-#     timestamp = DateField(id='dtpicker', label='Beginagain:')
+    # Search button
+    submit_button = SubmitField('Search')
 
 def create_app(configfile=None):
     app = Flask(__name__)
@@ -64,21 +28,12 @@ def create_app(configfile=None):
                                 # https://github.com/mbr/flask-appconfig
     Bootstrap(app)
 
-    # in a real app, these should be configured through Flask-Appconfig
+    # This should be configured through Flask-Appconfig
     app.config['SECRET_KEY'] = 'devkey'
 
     @app.route('/', methods=('GET', 'POST'))
     def index():
-        form = ExampleForm()
-        form.validate_on_submit()  # to get error messages to the browser
-        # flash('critical message', 'critical')
-        # flash('error message', 'error')
-        # flash('warning message', 'warning')
-        # flash('info message', 'info')
-        # flash('debug message', 'debug')
-        # flash('different message', 'different')
-        # flash('uncategorized message')
-
+        form = SearchForm()
         return render_template('index.html', form=form)
 
     return app
