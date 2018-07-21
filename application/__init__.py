a6d1d4b2e5b92891ab7c185f36bcb4a832e92f2f
jamesboyd2008 <jamesboyd2008@gmail.com>
2018-07-20 14:55:35 -1000

    Update flask_wtf.Form to FlaskForm.

--- application/__init__.py
+++ application/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, flash
 from flask_bootstrap import Bootstrap
 from flask_appconfig import AppConfig
-from flask_wtf import Form, RecaptchaField
+from flask_wtf import FlaskForm, RecaptchaField
 from flask_wtf.file import FileField
 from wtforms import TextField, HiddenField, ValidationError, RadioField,\
     BooleanField, SubmitField, IntegerField, FormField, validators, DateField,\
     SelectField
 from wtforms.validators import Required
 
-class SearchForm(Form):
+class SearchForm(FlaskForm):
     # Dropdowns
     x_pol = SelectField(label='X Pol Parameters', choices=[('a', 'apes'), ('b', 'bananas'), ('c', 'crabs')])
     y_pol = SelectField(label='Y Pol Parameters', choices=[('a', 'apes'), ('b', 'bananas'), ('c', 'crabs')])
